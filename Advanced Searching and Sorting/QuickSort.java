9bf39808d68271ccd45d1d07b817f251ec400c76
Pratyush ▲ <birole.pratyush@gmail.com>
2025-08-22 10:32:21 +0530

    Add files via upload

---
+++ Advanced Searching and Sorting/QuickSort.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class QuickSort {
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];  
+        int i = low - 1;        // index of smaller element
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // place pivot in correct position
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void solve(Scanner sc) {
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        quickSort(arr, 0, n - 1);
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            solve(sc);
+        }
+
+        sc.close();
+    }
+}
